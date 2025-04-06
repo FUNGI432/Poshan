371dc5d1822e811f6684affb225385cbb8feb29b
KhemkaAkshat <144114211+KhemkaAkshat@users.noreply.github.com>
2025-04-06 11:37:47 +0530

    feat(build) : setup

--- .next/server/app/api/medichatgemini/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/medichatgemini/route.js";
-const runtime = require("../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_66b75e._.js");
-runtime.loadChunk("server/chunks/[root of the server]__d4c7da._.js");
-runtime.loadChunk("server/chunks/_31cf4a._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/medichatgemini/route/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/medichatgemini/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"t4xrq53H56p0JkojJkjxDlFUkQCiqdK4qwxt6m8VlUI=\"\n}"
